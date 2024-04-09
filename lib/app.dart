3584803a7cabbd1a77cac962da2d9c2a61e23836
qurbonpulot <rustamqulovqurbonpulot@gmail.com>
2024-04-09 23:03:54 +0500

    intro.dart splash ended

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:melbook/presentation/intro.dart';
+import 'package:melbook/presentation/screens/home_screen.dart';
+
+bool haveUserAccount = false;
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: haveUserAccount? HomeScreen() : IntroScreen(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:melbook/app.dart';
+
 void main(){
-  
+  runApp(const App());
 }
