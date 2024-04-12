07a811d71049a8df0205d96471b88cbc0c46ed97
Yusupov Sherali <116561678+sheraliyusupov@users.noreply.github.com>
2024-04-12 10:58:03 +0500

    Slide id being

---
+++ lib/features/home/presentation/bookpage.dart
@@ -0,0 +1,15 @@
+import'package:flutter/material.dart';
+
+class Bookpage extends StatefulWidget {
+  const Bookpage({super.key});
+
+  @override
+  State<Bookpage> createState() => _BookpageState();
+}
+
+class _BookpageState extends State<Bookpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
