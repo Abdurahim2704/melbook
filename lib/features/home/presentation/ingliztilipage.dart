b67e7c987a8ffc1920e014774703b0c2e3a9326f
macbookpro <coderikhbolsheikh@gmail.com>
2024-04-13 17:03:01 +0500

    trash error fixed

--- lib/features/home/presentation/ingliztilipage.dart
+++ lib/features/home/presentation/ingliztilipage.dart
@@ -39,159 +39,156 @@ class _IngliztilipageState extends State<Ingliztilipage> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Colors.white,
       extendBodyBehindAppBar: true,
-      body: Expanded(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 60.h),
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(36),
-                    topLeft: Radius.circular(36),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const InsideBook(),
-                            ),
-                          );
-                        },
-                        child: CachedNetworkImage(
-                          imageUrl: widget.book.photoUrl,
-                          width: screenWidth * 0.25,
-                        ),
-                      ),
-                      const SizedBox(width: 13),
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.book.name,
-                            style: TextStyle(
-                              fontSize: 21.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Text(
-                            widget.book.author,
-                            style: TextStyle(
-                              fontSize: 15.sp,
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {},
-                            child: Container(
-                              padding: const EdgeInsets.all(11),
-                              margin: const EdgeInsets.only(top: 10),
-                              decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(70),
-                              ),
-                              child: Text(
-                                widget.book.bought ? "O'qish" : "Sotib olish",
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const CircleAvatar(
-                        radius: 24,
-                        backgroundColor: Color(0xFFF2F2F2),
-                        child: Icon(Icons.share, color: Colors.black),
-                      ),
-                    ],
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 60.h),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(36),
+                  topLeft: Radius.circular(36),
                 ),
               ),
-            ),
-            Container(
-              height: 580.h,
-              color: const Color(0xFFF2F2F2),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(height: 10.h),
-
-                  /// #Header PageView Buttons
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: _currentPage == 0
-                              ? Colors.black
-                              : Colors.transparent,
-                          fixedSize: Size(170.w, 30.h),
-                        ),
-                        onPressed: () => _changePage(0),
-                        child: Text(
-                          "Batafsil",
-                          style: TextStyle(
-                            color:
-                                _currentPage == 0 ? Colors.white : Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const InsideBook(),
                           ),
-                        ),
-                      ),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: _currentPage == 1
-                              ? Colors.black
-                              : Colors.transparent,
-                          fixedSize: Size(170.w, 30.h),
-                        ),
-                        onPressed: () => _changePage(1),
-                        child: Text(
-                          "Tinglab o'qish",
-                          style: TextStyle(
-                            color:
-                                _currentPage == 1 ? Colors.white : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  /// #PageView SubContent
-                  Expanded(
-                    child: PageView(
-                      controller: _pageController,
-                      onPageChanged: (page) {
-                        setState(() {
-                          _currentPage = page;
-                        });
+                        );
                       },
+                      child: CachedNetworkImage(
+                        imageUrl: widget.book.photoUrl,
+                        width: screenWidth * 0.25,
+                      ),
+                    ),
+                    const SizedBox(width: 13),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        BookDescriptionContainer(data: widget.book.description),
-                        ContainerAudiosListening(bookData: widget.book),
+                        Text(
+                          widget.book.name,
+                          style: TextStyle(
+                            fontSize: 21.sp,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          widget.book.author,
+                          style: TextStyle(
+                            fontSize: 15.sp,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(11),
+                            margin: const EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(70),
+                            ),
+                            child: Text(
+                              widget.book.bought ? "O'qish" : "Sotib olish",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                ],
+                    const CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Color(0xFFF2F2F2),
+                      child: Icon(Icons.share, color: Colors.black),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 580.h,
+            color: const Color(0xFFF2F2F2),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 10.h),
+
+                /// #Header PageView Buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: _currentPage == 0
+                            ? Colors.black
+                            : Colors.transparent,
+                        fixedSize: Size(170.w, 30.h),
+                      ),
+                      onPressed: () => _changePage(0),
+                      child: Text(
+                        "Batafsil",
+                        style: TextStyle(
+                          color:
+                              _currentPage == 0 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: _currentPage == 1
+                            ? Colors.black
+                            : Colors.transparent,
+                        fixedSize: Size(170.w, 30.h),
+                      ),
+                      onPressed: () => _changePage(1),
+                      child: Text(
+                        "Tinglab o'qish",
+                        style: TextStyle(
+                          color:
+                              _currentPage == 1 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                /// #PageView SubContent
+                Expanded(
+                  child: PageView(
+                    controller: _pageController,
+                    onPageChanged: (page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: [
+                      BookDescriptionContainer(data: widget.book.description),
+                      ContainerAudiosListening(bookData: widget.book),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
