8be3dfdfed891fe155530a05d8413815fd1ee0c9
qurbonpulot <rustamqulovqurbonpulot@gmail.com>
2024-04-14 08:32:24 +0500

    Smth

--- android/app/src/main/kotlin/com/melbook/melbook/MainActivity.kt
+++ android/app/src/main/kotlin/com/melbook/melbook/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.melbook.melbook
 
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.engine.FlutterEngine
+import android.view.WindowManager.LayoutParams
+class MainActivity: FlutterActivity(){
+    override  fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
